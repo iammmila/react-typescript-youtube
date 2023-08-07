e034672bbd73bcb3ddf277ac70bbe2dc02eaabcb
iammmila <ibramila2002@gmail.com>
2023-08-07 16:59:09 +0400

    basic props

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import "./App.css";
 import Greet from "./components/Greet";
+import Person from "./components/Person";
+import PersonList from "./components/PersonList";
 
 function App() {
+  const personName = { firstName: "Malaxat", lastName: "Iibrahimova" };
+  const nameList = [
+    { first: "birinci", last: "ikinci" },
+    { first: "ucuncu", last: "dorduncu" },
+    { first: "besinci", last: "altinci" },
+  ];
   return (
     <>
       <Greet name="malahat" currentAge={999} />
+      <Person name={personName} />
+      <PersonList names={nameList} />
     </>
   );
 }
